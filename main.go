2d8794ca92262bc954597a68db43c780f72a5377
Vladislav Kovyazin <vvkovyazin@miem.hse.ru>
2021-08-05 10:41:35 +0300

    change impl

--- main.go
+++ main.go
@@ -8,17 +8,9 @@ import (
 
 func main() {
 	src:= readInput()
-	wordcounter := make(map[string]int)
 	words := strings.Fields(src)
-	for _, word := range words {
-		if _, ok := wordcounter[word]; !ok {
-			wordcounter[word] = 0
-		}
-		wordcounter[word] += 1
-	}
-	for key, val := range wordcounter {
-		fmt.Println(key, ":", val)
-	}
+	fmt.Println(len(words))
+
 }
 
 func readInput() string {
